e09f332e0c79da882ce3af406dadadb046ca4967
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-08-09 23:33:04 +0900

    Title: 사칙연산 함수, Time: 197ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250809/사칙연산 함수/quadratic-operations-function.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [a, o, c] = input[0].split(" ");
+a = Number(a);
+c = Number(c);
+// Please Write your code here.
+
+const add = (a, c) => {
+    const result = a+c
+    return console.log(`${a} + ${c} = ${result}`)
+}
+
+const subtract = (a, c) => {
+    const result = a-c
+    return console.log(`${a} - ${c} = ${result}`)
+}
+
+const multiply = (a, c) => {
+    const result = a*c
+    return console.log(`${a} * ${c} = ${result}`)
+}
+
+const divide = (a, c) => {
+    const result = a/c
+    return console.log(`${a} / ${c} = ${result}`)
+}
+
+switch (o) {
+    case "+" :
+        add(a,c)
+        break;
+    case "-" :
+        subtract(a,c)
+        break;
+    case "*" :
+        multiply(a,c)
+        break;
+    case "/" :
+        divide(a,c)
+        break;
+}
