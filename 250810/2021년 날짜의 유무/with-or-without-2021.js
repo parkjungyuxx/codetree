2dd2c0c52f2a30718592c3d9cce7459a0632358a
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-08-10 23:26:30 +0900

    Title: 2021년 날짜의 유무, Time: 169ms, Memory: 15MB, Status: Passed - Codetree

--- 250810/2021년 날짜의 유무/with-or-without-2021.js
+++ 250810/2021년 날짜의 유무/with-or-without-2021.js
@@ -6,7 +6,7 @@ let [m, d] = input[0].split(" ").map(Number);
 const daysInMonth = [31,28,31,30,31,30,31,31,30,31,30,31]
 
 const solution = (m,d) => {
-    if (daysInMonth[m-1] > d) {
+    if (daysInMonth[m-1] >= d) {
         console.log("Yes")
     } else {
         console.log("No")
