b9377f7bf2569662d4e99499380d6686ae69bf61
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-08-11 15:43:19 +0900

    Title: 두 정수에 대한 연산값 2, Time: 182ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250811/두 정수에 대한 연산값 2/operational-values-for-two-integers-2.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+// Please Write your code here.
+
+const solution = (a,b) => {
+    if (a > b) {
+        a *= 2
+        b += 10
+    } else {
+        a += 10
+        b *= 2
+    }
+    return [a,b]
+}
+
+const [_a, _b] = solution(a,b)
+
+console.log(_a, _b)
