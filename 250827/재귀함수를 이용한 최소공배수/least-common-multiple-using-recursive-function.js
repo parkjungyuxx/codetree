332fb7a9f3238c1df6769290ae0c68da6c3f0931
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-08-27 17:22:47 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 378ms, Memory: 78MB, Status: Runtime Error - Codetree

---
+++ 250827/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+const numbers = input[1].split(" ").map(Number);
+// Please Write your code here.
+
+const gcd = (a,b) => {
+    if (b === 0) return a;
+    return gcd(b, a % b)
+}
+
+const lcm = (a,b) => {
+    return (a * b) / gcd(a,b)
+}
+
+const solution = (n) => {
+    if (n === 2) return lcm(numbers[0], numbers[1])
+
+    return lcm(solution(n-1), numbers[n-1]) 
+}
+
+console.log(solution(n))
