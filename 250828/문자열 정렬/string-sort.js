22130f9bc7b75e99bbf3a87e520529b4e4013513
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-08-28 23:57:24 +0900

    Title: 문자열 정렬, Time: 184ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250828/문자열 정렬/string-sort.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const s = input[0];
+// Please Write your code here. 
+
+console.log(s.split("").sort().join(""))
