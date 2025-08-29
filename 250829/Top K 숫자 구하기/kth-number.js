adc5558ce167f8b278d04165be93fe1acf71d1cf
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-08-29 21:09:10 +0900

    Title: Top K 숫자 구하기, Time: 168ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250829/Top K 숫자 구하기/kth-number.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+const nums = input[1].trim().split(' ').map(Number);
+
+// Please Write your code here.
+
+const arr = nums.sort()
+
+console.log(arr[k-1])
