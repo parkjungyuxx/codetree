1077c36ae2b4549b29db9bd447ff7beb930dc395
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-08-30 20:14:53 +0900

    Title: 비오는 날, Time: 193ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250830/비오는 날/rainy-day.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const forecasts = input.slice(1, n + 1).map(line => line.split(' '));
+
+// Please Write your code here.
+
+const filtered = forecasts.filter(el => el.includes("Rain"))
+
+const sorted = filtered.sort((a,b) => a[0] - b[0])
+
+class Weather {
+    constructor(date, dayOfWeek, weather) {
+        this.date = date;
+        this.dayOfWeek = dayOfWeek;
+        this.weather = weather
+    }
+}
+
+const result = new Weather(sorted[0][0], sorted[0][1], sorted[0][2])
+
+console.log(`${result.date} ${result.dayOfWeek} ${result.weather}`)
