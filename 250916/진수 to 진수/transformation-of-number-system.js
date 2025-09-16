8a7626c0792e6dea16517fdf228c881d1b2808ac
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-09-16 20:24:49 +0900

    Title: 진수 to 진수, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250916/진수 to 진수/transformation-of-number-system.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [a, b] = input[0].split(' ').map(Number);
+const n = input[1];
+// Please Write your code here.
+
+const num = parseInt(n,a)
+const result = num.toString(b)
+
+console.log(result)
