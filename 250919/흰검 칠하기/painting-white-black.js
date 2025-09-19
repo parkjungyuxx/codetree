507afec35be7353c1d97b2958ae36d245b55092f
micky <124859300+parkjungyuxx@users.noreply.github.com>
2025-09-19 21:43:41 +0900

    Title: 흰검 칠하기, Time: 84ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250919/흰검 칠하기/painting-white-black.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const MAX_K = 100000;
+let n = Number(input[0]);
+let a = Array(2 * MAX_K + 1).fill(0);
+let cntB = Array(2 * MAX_K + 1).fill(0);
+let cntW = Array(2 * MAX_K + 1).fill(0);
+let b = 0, w = 0, g = 0;
+
+let cur = MAX_K;
+for (let i = 1; i <= n; i++) {
+    let [x, c] = input[i].split(' ');
+    x = Number(x);
+
+    if (c === 'L') {
+        while (x > 0) {
+            a[cur] = 1;
+            cntW[cur] += 1;
+            x -= 1;
+
+            if (x) {
+                cur -= 1;
+            }
+        }
+    } else {
+        while (x > 0) {
+            a[cur] = 2;
+            cntB[cur] += 1;
+            x -= 1;
+
+            if (x) {
+                cur += 1;
+            }
+        }
+    }
+}
+
+for (let i = 0; i < 2 * MAX_K + 1; i++) {
+    if (cntB[i] >= 2 && cntW[i] >= 2) {
+        g += 1;
+    }
+    else if (a[i] === 1) {
+        w += 1;
+    } else if (a[i] === 2) {
+        b += 1;
+    }
+}
+
+console.log(`${w} ${b} ${g}`);
